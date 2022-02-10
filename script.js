838572c26e7a99445cbb032b3162b96ca8fafc57
Samarpit Nandanwar <96297906+Samarpeet17@users.noreply.github.com>
2022-02-10 18:48:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,106 @@
+const time = document.querySelector(".time");
+
+const interval = setInterval(() => {
+  const deadline = new Date(2022 ,1, 15, 12, 00, 00);
+  
+
+  const current = new Date();
+
+  const diff = deadline - current;
+
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24)) + "";
+  const hours = Math.floor((diff / (1000 * 60 * 60)) % 24) + "";
+  const minutes = Math.floor((diff / (1000 * 60)) % 60) + "";
+  const seconds = Math.floor((diff / 1000) % 60) + "";
+
+  time.innerHTML = `
+    <div data-content="Days">${days.length === 1 ? `0${days}` : days}</div>
+    <div data-content="Hours">${hours.length === 1 ? `0${hours}` : hours}</div>
+    <div data-content="Minutes">${
+      minutes.length === 1 ? `0${minutes}` : minutes
+    }</div>
+    <div data-content="Seconds">${
+      seconds.length === 1 ? `0${seconds}` : seconds
+    }</div>
+`;
+
+  if (diff < 0) {
+    clearInterval(interval);
+    time.innerHTML = "<h1>Here We Go!!!</h1>";
+  }
+
+  document.querySelector(".reset").addEventListener("click", () => {
+    clearInterval(interval);
+
+    const divs = document.querySelectorAll(".time div");
+
+    divs.forEach((div) => {
+      div.innerHTML = "00";
+    });
+  });
+}, 1000);
+
+
+
+
+
+// getting all required elements
+const searchWrapper = document.querySelector(".search-input");
+const inputBox = searchWrapper.querySelector("input");
+const suggBox = searchWrapper.querySelector(".autocom-box");
+const icon = searchWrapper.querySelector(".icon");
+let linkTag = searchWrapper.querySelector("a");
+let webLink;
+
+// if user press any key and release
+inputBox.onkeyup = (e)=>{
+    let userData = e.target.value; //user enetered data
+    let emptyArray = [];
+    if(userData){
+        icon.onclick = ()=>{
+            webLink = `https://www.google.com/search?q=${userData}`;
+            linkTag.setAttribute("href", webLink);
+            linkTag.click();
+        }
+        emptyArray = suggestions.filter((data)=>{
+            //filtering array value and user characters to lowercase and return only those words which are start with user enetered chars
+            return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
+        });
+        emptyArray = emptyArray.map((data)=>{
+            // passing return data inside li tag
+            return data = `<li>${data}</li>`;
+        });
+        searchWrapper.classList.add("active"); //show autocomplete box
+        showSuggestions(emptyArray);
+        let allList = suggBox.querySelectorAll("li");
+        for (let i = 0; i < allList.length; i++) {
+            //adding onclick attribute in all li tag
+            allList[i].setAttribute("onclick", "select(this)");
+        }
+    }else{
+        searchWrapper.classList.remove("active"); //hide autocomplete box
+    }
+}
+
+function select(element){
+    let selectData = element.textContent;
+    inputBox.value = selectData;
+    icon.onclick = ()=>{
+        webLink = `https://www.google.com/search?q=${selectData}`;
+        linkTag.setAttribute("href", webLink);
+        linkTag.click();
+    }
+    searchWrapper.classList.remove("active");
+}
+
+function showSuggestions(list){
+    let listData;
+    if(!list.length){
+        userValue = inputBox.value;
+        listData = `<li>${userValue}</li>`;
+    }else{
+      listData = list.join('');
+    }
+    suggBox.innerHTML = listData;
+}
+
